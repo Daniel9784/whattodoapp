46f590f6ebcd30d9690d7bd9516a0cb783eb99ac
Daniel <danielsagat1997@gmail.com>
2025-06-20 21:02:47 +0200

    Initial commit

---
+++ src/main/java/com/whattodo/whattodoapp/WhattodoappApplication.java
@@ -0,0 +1,13 @@
+package com.whattodo.whattodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhattodoappApplication.class, args);
+	}
+
+}
